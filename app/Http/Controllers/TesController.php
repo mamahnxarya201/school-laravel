6692e32eaf9c9788a7c15ef38ec1dacada8d72b8
diaz <diazcakasana201@gmail.com>
2020-09-13 12:49:44 +0700

    Tugas 1 laravel routing

---
+++ app/Http/Controllers/TesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesController extends Controller
+{
+    public function Show(){
+        return "Tes Controller berhasil";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Closure
+Route::get('/profil', function(){
+    return "halaman profile";
+})->name('profil');
+
+//Test Name Route
+Route::get('/tesname', function(){
+    return route('profil');
+});
+
+// Parameter
+Route::get('/profil/{id}', function($id){
+    return $id;
+});
+
+// Controller
+Route::get("/tescontroller", "TesController@Show");
+
+// Resource 
+Route::resource('siswa', "SiswaController");
